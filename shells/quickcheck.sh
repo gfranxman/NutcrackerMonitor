c9c82abc45bf41c501fb0e5d41d0cf37f323b879
Glenn Franxman <gfranxman@gmail.com>
2013-04-02 08:53:26 -0400

    some example scripts

---
+++ shells/quickcheck.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd ~/Projects/NutcrackerMonitor
+
+#./ballgazer.py --ignore ./ignorable_pools --unused app01.scrippsing.com
+./ballgazer.py --ignore ./ignorable_pools --unused app02.scrippsing.com
+./ballgazer.py --ignore ./ignorable_pools --unused app03.scrippsing.com
+./ballgazer.py --ignore ./ignorable_pools --unused app04.scrippsing.com
+./ballgazer.py --ignore ./ignorable_pools --unused app05.scrippsing.com
+./ballgazer.py --ignore ./ignorable_pools --unused app06.scrippsing.com
